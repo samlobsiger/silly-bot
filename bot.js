268b533d6858bc2dcdff3b3a796362e986000cb8
S.F.Lobsiger <sfl6@kent.ac.uk>
2019-02-12 13:31:50 +0000

    Refactored the javascript code so the query is answered i na seperate function

--- bot.js
+++ bot.js
@@ -1,10 +1,13 @@
+function answerQuery(message) {
+  if (message.indexOf("rain") != -1 || message.indexOf("sun") != -1 || message.indexOf("weather") != -1)
+    return "I do not care too much about weather I am stuck in a data center.";
+  return "Sorry Dave, I cannot do that.";
+}
 function saySomething() {
   var message = document.getElementById('message').value.toLowerCase();
   var conversation = document.getElementById('conversation');
-  var answer = "Sorry Dave, I cannot do that."
+  var answer = answerQuery(message);
 
-  if (message.indexOf("rain") != -1 || message.indexOf("sun") != -1 || message.indexOf("weather") != -1)
-    answer = "I do not care too much about weather, I'm locked inside a data center.";
 
   conversation.innerHTML += "<p><strong>You:</strong> " + message + "</p>";
   conversation.innerHTML += "<p><strong>Useless bot:</strong> " + answer + "</p>";
