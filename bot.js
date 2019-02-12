0150e0228c685eb9dab1eaa7c1f9f9a8855ea9bd
S.F.Lobsiger <sfl6@kent.ac.uk>
2019-02-12 13:22:44 +0000

    Fixed the javascript code because I missed a bracket before.

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ function saySomething() {
 
   if (message.indexOf("rain") != -1 || message.indexOf("sun") != -1 || message.indexOf("weather") != -1)
     answer = "I do not care too much about weather, I'm locked inside a data center.";
-  if (message.indexOf("tea") != -1 || message.indexOf("biscuit") != -1
+  if (message.indexOf("tea") != -1 || message.indexOf("biscuit") != -1)
 	answer = "I would love some tea and biscuits. Unfortunately I am a computer.";
 
   conversation.innerHTML += "<p><strong>You:</strong> " + message + "</p>";
